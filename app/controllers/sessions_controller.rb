aa94b33b69e96c61aa7981c65d69f8434d64e4e4
ypodo@yahoo.com <ypodo@yahoo.com>
2013-06-23 21:48:19 +0300

    Audio to speech

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user, :notice => "Signed in!"
        
-    else
+    elsif User.authenticate(params[:session][:email], params[:session][:password])
       user = User.authenticate(params[:session][:email], params[:session][:password])
       if user.nil?
         flash.now[:error] = "Invalid email/password combination, please try again."        
@@ -26,6 +26,8 @@ class SessionsController < ApplicationController
         redirect_to user
         #redirect_back_or user
       end
+    else
+      redirect_to root_url
     end
   end
   
