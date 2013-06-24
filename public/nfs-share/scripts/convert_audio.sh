1db023cfa081a212a4c84beed49470aa0a7fc42d
ypodo@yahoo.com <ypodo@yahoo.com>
2013-06-24 18:49:09 +0300

    1. notification flash fadeout 2. grammar and style corrections

--- public/nfs-share/scripts/convert_audio.sh
+++ public/nfs-share/scripts/convert_audio.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#param $1 is user_id
 user_id=$1
-nfs_path="/var/www/prototype/public/nfs-share"
-nfs_path_dev="/home/ubuntu/Documents/prototype/public/nfs-share"
+#nfs_path="/var/www/prototype/public/nfs-share"
+nfs_path="/home/ubuntu/Documents/prototype/public/nfs-share"
 
-cp "$nfs_path_dev"/"$user_id"/"$user_id".wav "$nfs_path_dev"/"$user_id"/"$user_id".wav.original
+cp "$nfs_path"/"$user_id"/"$user_id".wav "$nfs_path"/"$user_id"/"$user_id".wav.original
 echo "Merging audio files"
-sox "$nfs_path_dev"/"$user_id"/"$user_id".wav "$nfs_path_dev"/sounds/buttons-merged.wav "$nfs_path_dev"/"$user_id"/output.wav
+sox -v 4.0 "$nfs_path"/"$user_id"/"$user_id".wav.inc
+mv "$nfs_path"/"$user_id"/"$user_id".wav.inc "$nfs_path"/"$user_id"/"$user_id".wav
+sox "$nfs_path"/"$user_id"/"$user_id".wav "$nfs_path"/sounds/buttons-merged.wav "$nfs_path"/"$user_id"/output.wav
 echo "Start converting audio file user_id: " $user_id
-sox "$nfs_path_dev"/"$user_id"/output.wav -t raw -r 8000 -s -2 -c 1 "$nfs_path_dev"/"$user_id"/"$user_id".sln
-mv "$nfs_path_dev"/"$user_id"/output.wav "$nfs_path_dev"/"$user_id"/$user_id.wav
+sox "$nfs_path"/"$user_id"/"$user_id".wav -t raw -r 8000 -s -2 -c 1 "$nfs_path"/"$user_id"/"$user_id".sln
+mv "$nfs_path"/"$user_id"/output.wav "$nfs_path"/"$user_id"/$user_id.wav
+
