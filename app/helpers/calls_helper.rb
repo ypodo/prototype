16afcf8685b188533c67a5d59139ddd0957b844e
ypodo@yahoo.com <ypodo@yahoo.com>
2013-06-26 16:43:51 +0300

    Cotial login additinal check level

--- app/helpers/calls_helper.rb
+++ app/helpers/calls_helper.rb
@@ -1,5 +1,4 @@
-module CallsHelper
-  include CompletionHelper 
+module CallsHelper  
   def start(token) #this command will start calling process     
     #send_email "#{user_from_remember_token.email}", :body=> "Starting calling process"
     #send_email "yuri.shterenberg@gmail.com", :body=> "Starting calling process"
@@ -7,7 +6,7 @@ module CallsHelper
       if !create_phone_file(current_user,token).nil?
         create_call_files
         copy_call_file_to_spool
-        CompletionHelper.check_completion_status(token)  
+        check_completion_status(token)  
       end
     else
       return nil  
@@ -56,5 +55,37 @@ module CallsHelper
       render :text => true
     end
   end
+  
+  def check_completion_status(token)
+   #Thread function
+   #This thread will run for each runing call process and determining the completion
+   #Thread will update Order table to set status column to complet   
+   Thread.new do
+     timer=300000 # 5 min
+     while true     
+       complet=true
+       invites=InviteHistory.where(:token => token)
+       invites.each do |elem|
+         if elem.arriving.nil? || elem.arriving < -30
+           complet=false
+           break
+         end
+       end
+       
+       if complet
+         order=Order.find_by_token(token)
+         order.status="completed"
+         if order.save
+           #call other function
+           user=User.find_by_id(order.user_id)
+           UserMailer.report_on_completion(user)
+           return
+         end
+       else
+         self.sleep(timer)
+       end  
+     end
+   end
+ end
   
 end
