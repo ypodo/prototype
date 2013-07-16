4c42448870d082da18d158df9f84907479526e61
ypodo@yahoo.com <ypodo@yahoo.com>
2013-07-16 17:54:20 +0300

    Minimum changed to 2

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,3 +1,16 @@
 module OrdersHelper
-  
+  def notify_the_invited(token)
+    begin            
+      if !token.nil?      
+        invite_yes=InviteHistory.where(:token=>token,:arriving=>"1")
+        invite_no=InviteHistory.where(:token=>token,:arriving=>"2")
+        UserMailer.notify_the_invited(:invite=>invite_yes,:recipients=>mail_list_yes)
+        UserMailer.notify_the_invited(:invite=>invite_no,:recipients=>mail_list_no)
+      end  
+    rescue Exception => e
+      logger.error { "message: #{e}" }
+    end    
+  end
 end
+
+
